0d4c4f8522ed4aa2d901131735882f599f037c5c
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-05-04 10:52:40 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,7 @@
+    <footer class="site-footer">
+        <small>&copy; <?php echo date('Y'); ?> Digital Voting System. Final Year Project.</small>
+    </footer>
+    <script src="<?php echo h($asset_base ?? ''); ?>js/main.js"></script>
+</body>
+</html>
 
