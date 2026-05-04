e8dc3ce97e7def44a4a6a93b82233aa649e2f4cf
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-05-04 10:53:22 +0530

    Update session.php

--- includes/session.php
+++ includes/session.php
@@ -1,1 +1,21 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/../php/functions.php';
+
+if (!isset($_SESSION['last_activity'])) {
+    $_SESSION['last_activity'] = time();
+}
+
+if (time() - $_SESSION['last_activity'] > 1800) {
+    session_unset();
+    session_destroy();
+    $login_path = strpos($_SERVER['SCRIPT_NAME'] ?? '', '/admin/') !== false ? '../login.html' : 'login.html';
+    header('Location: ' . $login_path);
+    exit();
+}
+
+$_SESSION['last_activity'] = time();
+?>
 
