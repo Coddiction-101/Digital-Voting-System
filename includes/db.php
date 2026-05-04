a7a28179a526d3a113b864efb112a34058dcaf38
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-05-04 10:52:30 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,18 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'voting_system';
+
+$conn = mysqli_connect($host, $user, $password, $database);
+
+if (!$conn) {
+    error_log('Database connection failed: ' . mysqli_connect_error());
+    die('Database connection failed. Please try again later.');
+}
+
+mysqli_set_charset($conn, 'utf8mb4');
+?>
 
